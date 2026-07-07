c6d632becb9df41941a7ebb89ed3f4fda103205f
GiliMinkovitz <gili0504126522@gmail.com>
2026-07-07 13:13:13 +0300

    Improve VS Code CMake/build tasks and add stdin prompt to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <string>
 
 int main() {
     std::cout << "Kung Fu Chess\n";
+    std::cout << "Enter something: ";
+
+    std::string input;
+    std::getline(std::cin, input);
+
+    std::cout << "You entered: " << input << "\n";
     return 0;
 }
