ff13e4119783715f6db437eb3484d1f17dd2e977
GiliMinkovitz <gili0504126522@gmail.com>
2026-07-14 19:26:52 +0300

    build: fix missing UI source files and headers
    - Added missing src/ui/i_ui_input_sink.h interface definition.
    - Added empty implementation for HeadlessRenderer in src/ui/headless_renderer.cpp to satisfy CMake build requirements.
    - Resolved build failures for KungFuChessGui and test targets.

---
+++ src/ui/i_ui_input_sink.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace kfc {
+
+class IUiInputSink {
+public:
+    virtual ~IUiInputSink() = default;
+    virtual void on_pixel_click(int x, int y) = 0;
+    virtual void on_pixel_jump(int x, int y) = 0;
+};
+
+}  // namespace kfc
